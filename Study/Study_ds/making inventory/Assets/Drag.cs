241e4254e6f7c5c6fd675e1747d615085acd4b1c
0WILLE0 <wille5852@gmail.com>
2018-09-03 14:04:38 +0900

    인벤토리 on-off 기능 추가

--- Study/Study_ds/making inventory/Assets/Drag.cs
+++ Study/Study_ds/making inventory/Assets/Drag.cs
@@ -9,27 +9,31 @@ public class Drag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHand
     private Transform inventory;
     private Transform Holdingitem;
     private CanvasGroup canvasGroup;
+    private Transform Slot;
 
     public static GameObject draggingItem = null;
 
+    
     void Start()
     {
         itemTr = GetComponent<Transform>();
         inventory = GameObject.Find("Inventory").GetComponent<Transform>();
-        Holdingitem = GameObject.Find("Holding_Items").GetComponent<Transform>();
- 
-        //Canvas Group 컴포넌트 추출
+        
+        Slot = GameObject.Find("slot").GetComponent<Transform>();
+
+        Holdingitem = transform.parent;
+        
         canvasGroup = GetComponent<CanvasGroup>();
     }
 
-    
+
     public void OnDrag(PointerEventData eventData)
     {
         //마우스가 아이템을 끌어들이게 변경.
         itemTr.position = Input.mousePosition;
     }
 
-    
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         //인벤토리로 부모 갈아타기
@@ -38,9 +42,11 @@ public class Drag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHand
         draggingItem = this.gameObject;
         //다른 이벤트 차단
         canvasGroup.blocksRaycasts = false;
+        
+       
     }
 
-    
+
     public void OnEndDrag(PointerEventData eventData)
     {
         //저장했던 정보 삭제
@@ -49,7 +55,7 @@ public class Drag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHand
         canvasGroup.blocksRaycasts = true;
 
         //슬롯에 놓은게 아니면 원래 제자리로 돌려보냄.
-        if (itemTr.parent == inventory)
+        if ((itemTr.parent == inventory))
         {
             itemTr.SetParent(Holdingitem.transform);
         }
---
+++ Study/Study_ds/making inventory/Assets/GMscript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMscript : MonoBehaviour {
+
+    public CanvasGroup invenCG;//인벤 켄버스그룹 구성성분 저장변수
+	void Start () {
+        InvenOpen(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void InvenOpen(bool open)//인벤 활성/비활성화 함수
+    {
+        invenCG.alpha = (open) ? 1.0f : 0.0f;
+        invenCG.interactable = open;
+        invenCG.blocksRaycasts = open;
+    }
+}
