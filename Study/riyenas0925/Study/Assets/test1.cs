00b4f761ae2dd4913682aa529df8cc85e178b1d5
riyenas0925 <riyenas0925@gmail.com>
2018-11-20 20:33:33 +0900

    riyenas0925 캐릭터 전환

---
+++ Study/riyenas0925/Study/Assets/test1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test1 : MonoBehaviour
+{
+    public int Speed;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (test2.KeyState == false)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(Vector3.left * Speed * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(Vector3.right * Speed * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(Vector3.back * Speed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Study/riyenas0925/Study/Assets/test2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class test2 : MonoBehaviour {
+
+    public int Speed;
+    public static bool KeyState;
+
+    // Use this for initialization
+    void Start () {
+        KeyState = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(KeyState == true)
+        {
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(Vector3.left * Speed * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(Vector3.right * Speed * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(Vector3.back * Speed * Time.deltaTime);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            test2.KeyState = !test2.KeyState;
+        }
+    }
+}
