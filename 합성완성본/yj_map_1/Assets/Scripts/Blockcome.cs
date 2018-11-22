4b70473798e4c4cf8d87a09f820a9316499a8ec8
DESKTOP-1MR96DQ\황유진 <hip1998@naver.com>
2018-11-23 01:00:08 +0900

    테트리스 화면 수정해주세요~

--- 합성완성본/yj_map_1/Assets/Scripts/Blockcome.cs
+++ 합성완성본/yj_map_1/Assets/Scripts/Blockcome.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //tap키를 눌러 블럭을 생성 할 수 있는 스크립트 입니다. 플레이어가 플레이존에 없을땐 비활성화 되있습니다
-public class Blockcome : MonoBehaviour {
+public class Blockcome : MonoBehaviour
+{
 
     GameObject[] blockk;
     public GameObject Block1;
@@ -12,8 +13,11 @@ public class Blockcome : MonoBehaviour {
     public GameObject Block4;
     public GameObject Block5;
 
+    Vector3 whereme;
+    GameObject blockcomeme;
 
-    void Start () {
+    void Start()
+    {
         // Block = new GameObject[5];
         blockk = new GameObject[5];
         blockk[0] = Block1;
@@ -22,6 +26,8 @@ public class Blockcome : MonoBehaviour {
         blockk[3] = Block4;
         blockk[4] = Block5;
 
+        whereme = this.gameObject.transform.position;
+        blockcomeme = this.gameObject;
         /*StartCoroutine("Blockking");*/
     }
 
@@ -36,16 +42,28 @@ public class Blockcome : MonoBehaviour {
         }
     }*/
 
+    void Update()
+    {
+        //씨드 설정
+        Random.seed = (int)System.DateTime.Now.Ticks;
+        Debug.Log("time is " + Random.seed);
 
-    void Update () {
-        //tap키 누르면 블럭 생성
-        if (Input.GetKeyDown(KeyCode.Tab))
+        //블럭 생성
+        int i;
+        i = Random.Range(0, 5);
+        Instantiate(blockk[i], whereme, Quaternion.identity);
+        Collider[] colls = Physics.OverlapSphere(blockk[i].transform.position, 50.0f);
+        foreach (Collider coll in colls)
+        {
+            Rigidbody rbody = coll.GetComponent<Rigidbody>();
+            if (rbody != null)
             {
-                int i;
-                i = Random.Range(0, 5);
-                Instantiate(blockk[i], new Vector3(-15, 85, -120), Quaternion.identity);
-                Debug.Log("you push TAP and object name is " + this.gameObject.name);
-                this.gameObject.GetComponent<Blockcome>().enabled = false;
+                rbody.mass = 1.0f;
+                rbody.AddExplosionForce(1000.0f, blockk[i].transform.position, 10.0f, 300.0f);
             }
+        }
+        //Instantiate(blockk[i], new Vector3(-15, 85, -120), Quaternion.identity);
+        Debug.Log("you push TAP and object name is " + this.gameObject.name);
+        blockcomeme.SetActive(false);
     }
 }
